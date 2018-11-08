f328176f12cce02d435892aa002f8ca41cac3bc3
jahnavip9 <jahnavip@ussoftwarepro.com>
2018-11-08 15:46:57 +0530

    Update colorplot.m

--- colorplot.m
+++ colorplot.m
@@ -1,16 +1,20 @@
-function colorplot(n,d)
+function colorplot(n)
+%%COLORPLOT Plots increasing and decreasing sections of a plot in green and
+%%red respectively
+% INPUTS :- 
+%        n - Timetable (Nx1)
 
-datay = diff(n);
+datay = diff(n.Value);
 
 low= datay >0;
 high  = datay <0;
-
     
- x=[d(1:end-1) d(2:end)]';
- y=[n(1:end-1) n(2:end)]';
+ x=[n.Date(1:end-1) n.Date(2:end)]';
+ y=[n.Value(1:end-1) n.Value(2:end)]';
  
 plot(x(:,high),y(:,high),'g--',x(:,low),y(:,low),'r--');
 xlabel('Date');
 ylabel('Value');
 
 end
+
