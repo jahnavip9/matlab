83705558009f39a1e9d1eeb31e6a38195b4d4f54
jahnavip9 <jahnavip@ussoftwarepro.com>
2018-12-04 16:33:14 +0530

    Update and rename even.m to frevn.m

--- frevn.m
+++ frevn.m
@@ -1,10 +1,20 @@
 %% Displaying even numbers using loops and conditions
 n1=0;  
 n2=50000; 
-foreven(n1,n2)% calling foreven function
-
+tic
+for m=1:10000
+foreven(n1,n2);% calling foreven function
+end
+t1=toc;
 %% Displaying even numbers without using loops and conditions
 x1= 0;
 n= 2;
 x2= 50000;
-dispeven(x1,n,x2)% calling dispeven function
+tic
+for k=1:10000
+dispeven(x1,n,x2);% calling dispeven function
+end
+t2=toc;
+
+
+
