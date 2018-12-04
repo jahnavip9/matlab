b6384afa8dc27116b20de41f3c04837e799370ff
jahnavip9 <jahnavip@ussoftwarepro.com>
2018-12-04 16:34:46 +0530

    Update foreven.m

--- foreven.m
+++ foreven.m
@@ -1,14 +1,16 @@
-%function for displaying even numbers using loops and conditions
-function foreven (n1,n2)
+function [even]= foreven(n1,n2)
 %%foreven displays all the even numbers between specific range
 % INPUTS : 
 %        n1 - starting range
 %        n2 - ending range
 % OUTPUT :
-%        j - gives the even numbers between the given range
-for j=n1:n2 % \ for j=n1,j<=n2,j++
+%        even - gives the even numbers between the given range
+even =zeros(1,floor((n2-n1))/2);
+i =1;
+for j=n1:n2 
 if mod(j,2) == 0 
-    disp(j)
+even(i)= j;
+i=i+1;
 end
 end
 end
